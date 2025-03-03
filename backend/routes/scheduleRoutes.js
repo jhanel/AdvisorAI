35095573e2043dc1a049b103b02bff0c163bae76
jhanel <72320010+jhanel@users.noreply.github.com>
2025-03-03 13:10:41 -0500

    Update scheduleRoutes
    Added edit api to scheduleRoutes.js

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -25,3 +25,38 @@ router.post('/search', async (req, res) => {
 });
 
 module.exports = router;
+
+// Edit API
+
+router.post('/edit', async (req, res) => 
+{
+    try
+    {
+        const { userId, scheduleId, newSchedule } = req.body;
+
+        if ( !userId || !scheduleId || !newSchedule )
+        {
+            return res.status(400).json({ error: 'Missing required field(s).' });
+        }
+
+        const updatedSchedule = await Schedule.findOneAndUpdate(
+            { _id: scheduleId, userId },    // match scheduleId & userId
+            { $set: { schedule: newSchedule } },
+            { new: true }
+        );
+
+        if( !updatedSchedule )
+        {
+            return res.status(404).json({ error: 'Schedule not found.' });
+        }
+
+        res.status(200).json({ error: 'Schedule updated successfully!', updatedSchedule });
+
+    } catch ( error ) 
+    {
+        res.status(500).json({ error: 'Error updating schedule.' });
+    }
+
+});
+
+module.exports = router;
