129c91674baed37ad4d6313b42a9d23c2f1cd8e9
Gabbie <gabbiecoronel@gmail.com>
2025-03-05 17:32:39 -0500

    Fixing typos

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {useNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("MongoDB Connected!"))
     .catch(err => console.error("MongoDB Connection Error:", err));
 
@@ -31,4 +31,4 @@ app.use((req, res, next) =>
   next();
 });
 
-app.listen(5002, '0.0.0.0',); 
+app.listen(5002, '0.0.0.0'); 
