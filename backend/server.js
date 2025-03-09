fd6fb33c40b50d2f802bf8990167e8d46d112729
Gabbie <gabbiecoronel@gmail.com>
2025-03-08 21:16:28 -0500

    Moving dotenv code at top of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
