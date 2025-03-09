fbaf2138b01c2c891cf9dc11157ec1217ca91dcc
Gabbie <gabbiecoronel@gmail.com>
2025-03-08 21:41:55 -0500

    API Endpoints Work on the Server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 require('dotenv').config({ path: './.env' });
-console.log("MongoDB URI:", process.env.MONGO_URI);
 const mongoose = require('mongoose');
-//require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -11,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected!"))
     .catch(err => console.error("MongoDB Connection Error:", err));
 
