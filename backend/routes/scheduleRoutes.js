670a68c2c1a4e73dd5ed608464ff4e5e8f3056b7
jhanel <72320010+jhanel@users.noreply.github.com>
2025-03-26 12:56:44 -0400

    Update scheduleRoutes.js
    Deleted Search and Edit API from

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -105,62 +105,6 @@ router.patch("/verifyemail", async (req, res) => {
   
     return res.status(200).json({ status: "Success", message: "User verified successfully" });
   });
-  
-
-// Search API
-
-router.post('/search', async (req, res) => {
-    try
-    {
-        const { userId } = req.body;
-        if ( !userId ) return res.status(400).json({ error: 'User ID is required.' });
-
-        const schedules = await Schedule.find({ userId });
-
-        if( schedules.length == 0 )
-        {
-            return res.status(404).json({ message: 'No schedule found.' });
-        }
-
-        res.status(200).json({ schedules });
-    } catch ( error )
-    {
-        res.status(500).json({ error: 'Error retrieving schedule.' });
-    }
-});
-
-// Edit API
-
-router.post('/edit', async (req, res) => 
-{
-    try
-    {
-        const { userId, scheduleId, newSchedule } = req.body;
-
-        if ( !userId || !scheduleId || !newSchedule )
-        {
-            return res.status(400).json({ error: 'Missing required field(s).' });
-        }
-
-        const updatedSchedule = await Schedule.findOneAndUpdate(
-            { _id: scheduleId, userId },    // match scheduleId & userId
-            { $set: { schedule: newSchedule } },
-            { new: true }
-        );
-
-        if( !updatedSchedule )
-        {
-            return res.status(404).json({ error: 'Schedule not found.' });
-        }
-
-        res.status(200).json({ error: 'Schedule updated successfully!', updatedSchedule });
-
-    } catch ( error ) 
-    {
-        res.status(500).json({ error: 'Error updating schedule.' });
-    }
-
-});
 
 // Sends email for password reset
 router.post('/passwordreset', async (req, res) => {
