20034b02799d260dc27456acedfb7d271eb714e9
Annabel-S <136514085+Annabel-S@users.noreply.github.com>
2025-03-28 22:15:21 -0400

    course

--- backend/models/course.js
+++ backend/models/course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
     coursetitle: { type: String, required: true },
     difficulty: { 
         type: String, 
